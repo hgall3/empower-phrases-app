80925e6f0c532a312d3484f95793508d29b6e15f
Montc027 <lymomuca@gmail.com>
2025-07-31 12:56:03 +0200

    created the add phrase form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './styles/main.scss'
 import PhraseList from './components/PhraseList'
+import AddPhraseForm from './components/AddPhraseForm.jsx'
 import { initialPhrases } from './assets/data/data.js'
+
 
 
 function App() {
   const [phrases, setPhrases] = useState(initialPhrases)
+
+  const handleAddPhrase = (newPhrase) => {
+    setPhrases(prev => [...prev, newPhrase]);
+  };
 
   return (
     <>
@@ -16,6 +22,7 @@ function App() {
       <section className='box-global'>
         <img className='img-left' src="src/assets/flowers_right.png" alt="flowers-background-with-moth" />
         <div className="phrases-container">
+          <AddPhraseForm onAdd={handleAddPhrase} />
           <PhraseList phrases={phrases} />
         </div>
         <img className='img-right' src="src/assets/flowers_left.png" alt="flowers-background" />
@@ -28,4 +35,6 @@ function App() {
   )
 }
 
-export default App
+
+
+export default App;
---
+++ src/components/AddPhraseForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./AddPhraseForm.scss"
+
+
+function AddPhraseForm({onAdd}) {
+    const [text, setText] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!text.trim()) return alert("Phrase cannot be empty");
+            onAdd({ id: Date.now(), text: text.trim(), author: author.trim() || "Anonymous" });
+            setText("");
+            setAuthor("");
+    };
+
+    return (
+        <form className="add-phrase-form" onSubmit={handleSubmit}>
+            <h2>Inspiration Quotes</h2>
+            <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Write your pharse..."
+            rows={3}
+            required
+            />
+            <input type="text"
+            value={author}
+            onChange={(e) => setAuthor (e.target.value)}
+            placeholder="Author (opcional)" 
+            />
+            <button type="submit"Add Phrase></button>
+            </form>
+    );
+
+}
+
+export default AddPhraseForm;
+
