fce8b968504fef3abfe339d0f87e4ad10c6ec478
Daniella Pacheco <daniellak.pacheco@gmail.com>
2025-08-01 10:19:42 +0200

    Fixing import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './styles/main.scss'
-import PhraseList from './components/phrase-list/PhraseList.jsx'
+import PhraseList from './components/PhraseList.jsx'
 import { initialPhrases } from './assets/data/data.js'
 
 
