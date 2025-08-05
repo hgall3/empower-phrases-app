0a54af9ec634affc868b2eca5695889e57cd3fd4
DevErika <erikamontoya2023@gmail.com>
2025-08-05 09:54:26 +0200

    Create folders to add image to quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import './styles/main.scss'
 import PhraseList from './components/PhraseList'
-import AddPhraseForm from './components/AddPhraseForm.jsx'
+/*import AddPhraseForm from './components/AddPhraseForm.jsx' //He comentado esta línea*/
 import { initialPhrases } from './assets/data/data.js'
-
-
+import AddImageQuote from './components/addImageToQuote/AddImageQuote.jsx'; //He agregado esta línea (Erika)
 
 function App() {
   const [phrases, setPhrases] = useState(initialPhrases)
@@ -22,8 +21,9 @@ function App() {
       <main className='box-global'>
         <img className='img-left' src="src/assets/flowers_right.png" alt="flowers background with moth" />
         <div className="phrases-container">
-          <AddPhraseForm onAdd={handleAddPhrase} />
+        {/* <AddPhraseForm onAdd={handleAddPhrase} />*/}
           <PhraseList phrases={phrases} />
+          <AddImageQuote />
         </div>
         <img className='img-right' src="src/assets/flowers_left.png" alt="flowers background" />
        </main>
