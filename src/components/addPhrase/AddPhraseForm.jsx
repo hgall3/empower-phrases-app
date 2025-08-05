52574b20603838a8071302d66f63e5fe310366b1
Andrea <aoliverar.96@gmail.com>
2025-08-05 15:16:09 +0200

    modal added to add phrase

--- src/components/addPhrase/AddPhraseForm.jsx
+++ src/components/addPhrase/AddPhraseForm.jsx
@@ -1,39 +1,50 @@
 import { useState } from "react";
-import "./AddPhraseForm.scss"
+import "./AddPhraseForm.scss";
+import Modal from "../warningModal/ModalFillQuote";
 
-
-function AddPhraseForm({onAdd}) {
+function AddPhraseForm({ onAdd }) {
     const [text, setText] = useState("");
     const [author, setAuthor] = useState("");
+    const [showModal, setShowModal] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!text.trim()) return alert("Phrase cannot be empty");
-            onAdd({ id: Date.now(), text: text.trim(), author: author.trim() || "Anonymous" });
-            setText("");
-            setAuthor("");
+
+        if (!text.trim()) {
+            setShowModal(true);
+            return;
+        }
+        onAdd({ id: Date.now(), text: text.trim(), author: author.trim() || "Anonymous" });
+        setText("");
+        setAuthor("");
     };
 
     return (
-        <form className="add-phrase-form" onSubmit={handleSubmit}>
-            {/* <h2>Inspiration Quotes</h2> */}
-            <textarea
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Write your pharse..."
-            rows={3}
-            required
-            />
-            <input type="text"
-            value={author}
-            onChange={(e) => setAuthor (e.target.value)}
-            placeholder="Author (opcional)" 
-            />
-            <button type="submit">Add Phrase</button>
-        </form>
+        <>
+            <form className="add-phrase-form" onSubmit={handleSubmit}>
+                <textarea
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Write your phrase..."
+                    rows={3}
+                />
+                <input
+                    type="text"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                    placeholder="Author (optional)"
+                />
+                <button type="submit">Add Phrase</button>
+            </form>
+            
+            {showModal && (
+                <Modal
+                    message="Please fill out the phrase before adding."
+                    onClose={() => setShowModal(false)}
+                />
+            )}
+        </>
     );
-
 }
 
 export default AddPhraseForm;
-
