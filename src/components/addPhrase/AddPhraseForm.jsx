b552254ccc2d4d13757a099dfceaab8924d7db57
Andrea <aoliverar.96@gmail.com>
2025-08-05 15:32:38 +0200

    merge from developer

--- src/components/addPhrase/AddPhraseForm.jsx
+++ src/components/addPhrase/AddPhraseForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./AddPhraseForm.scss";
 import Modal from "../warningModal/ModalFillQuote";
+import newbutton from "../../assets/iconadd.svg"
+
 
 function AddPhraseForm({ onAdd }) {
     const [text, setText] = useState("");
@@ -22,20 +24,26 @@ function AddPhraseForm({ onAdd }) {
     return (
         <>
             <form className="add-phrase-form" onSubmit={handleSubmit}>
-                <textarea
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    placeholder="Write your phrase..."
-                    rows={3}
-                />
-                <input
-                    type="text"
-                    value={author}
-                    onChange={(e) => setAuthor(e.target.value)}
-                    placeholder="Author (optional)"
-                />
-                <button type="submit">Add Phrase</button>
-            </form>
+                <label htmlFor="phrase-input">Your phrase:</label>
+            <textarea
+            id="phrase-input"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Write your pharse..."
+            rows={3}
+            />
+
+            <label htmlFor="author-input">Author (optional): </label>
+            <input 
+            id="author-input"
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor (e.target.value)}
+            placeholder="Author (optional)" 
+            />
+
+            <button className="buttoncard" type="submit"><img src={newbutton} alt="" /></button> 
+        </form>
             
             {showModal && (
                 <Modal
